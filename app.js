485c6a645216c6f58762c974132a085148c59dc1
Anita Kamani <anitakamani@outlook.com>
2022-09-16 00:23:47 +0430

    connected js to the html elements

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
-// set the current date
-var now = new Date();
+while (true) {
+	// set the current date
+	var now = new Date();
 
-// set the target date (next year's eve)
-var target_year = now.getFullYear() + 1;
-var target_month = 0; // Januar
-var target_day = 1;
-var target = new Date(target_year, target_month, target_day);
+	// set the target date (next year's eve)
+	var target_year = now.getFullYear() + 1;
+	var target_month = 0; // Januar
+	var target_day = 1;
+	var target = new Date(target_year, target_month, target_day);
 
-var diffrence = Math.floor((target - now) / 1000);
-// console.log(diffrence);
+	var diffrence = Math.floor((target - now) / 1000);
+	// console.log(diffrence);
 
-var p = 24 * 60 * 60;
-var left_days = Math.floor(diffrence / p);
-var left_hours = Math.floor((diffrence % p) / (60 * 60));
-p /= 24;
-var left_minutes = Math.floor((diffrence % p) / 60);
-p /= 60;
-var left_seconds = Math.floor(diffrence % p);
+	var p = 24 * 60 * 60;
+	var left_days = Math.floor(diffrence / p);
+	var left_hours = Math.floor((diffrence % p) / (60 * 60));
+	p /= 24;
+	var left_minutes = Math.floor((diffrence % p) / 60);
+	p /= 60;
+	var left_seconds = Math.floor(diffrence % p);
 
-console.log(`${left_days} days left `);
-console.log(`${left_hours} hours left`);
-console.log(`${left_minutes} minutes left`);
-console.log(`${left_seconds} seconds left`);
+	console.log(`${left_days} days left `);
+	console.log(`${left_hours} hours left`);
+	console.log(`${left_minutes} minutes left`);
+	console.log(`${left_seconds} seconds left`);
 
-export { left_days, left_hours, left_minutes, left_seconds };
+	document.getElementById("day").innerHTML = left_days;
+	document.getElementById("hour").innerHTML = left_hours;
+	document.getElementById("minute").innerHTML = left_minutes;
+	document.getElementById("second").innerHTML = left_seconds;
+
+	// reload timeout
+	await new Promise((r) => setTimeout(r, 1000));
+}
